72a6d058653717415f56d0c68ee302c69dd0b275
Sergey Kucheryavskiy <svkucheryavski@gmail.com>
2022-12-01 13:57:30 +0100

    small improvements to demo.m

--- demo.m
+++ demo.m
@@ -1,9 +1,13 @@
-% This is demo code for the new version of the Procrustes 
+% This is demo code for the new version of the Procrustes
 % cross-validation method (2022)
 %
-% The code creates plots similar to what you can see on Figures with Corn example
-% in the paper describing the new version, submitted in 2022. The Corn dataset was
-% downloaded from here: https://eigenvector.com/resources/data-sets/
+% In order to use the code you need to download and install the "Procrustes cross-validation"
+% toolbox for MATLAB. You can do it from the public repository:
+% https://se.mathworks.com/matlabcentral/fileexchange/121468-procrustes-cross-validation
+% or by donwloading the .mtlbx file from GitHub (see Releases)
+%
+% The code below creates plots similar to what you can see on Figures with Corn example
+% in the paper describing the new version, submitted in 2022.
 %
 
 % load Corn data
@@ -14,7 +18,7 @@ load("corn.mat");
 
 %% 1. PCA based examples
 
-A = 30;
+A = 20;
 I = size(X, 1);
 
 % create pseudo-validation set
@@ -38,73 +42,34 @@ subplot 224
 plot((Xpv - mean(Xpv, 1))')
 title("PV-set (mean centered)")
 
+
 % mean center calibration and PV sets
 mX = mean(X);
 Xmc = X - mX;
 Xpvmc = Xpv - mX;
 
-% make PCA model for the calibration set
+% get PCA loadings for the calibration set
 [~, ~, P] = svd(Xmc);
 P = P(:, 1:A);
+
+% compute eigenvalues - we need them for score distances
 T = Xmc * P;
-lambda = sum(T.^2) / (I - 1);
-
-% get scores for the PV-set
-Tpv = Xpvmc * P;
-
-% compute scores and distances for both calibration set and PV-set 
-% for a = 1 to A
-
-q = zeros(I, A);
-h = zeros(I, A);
-
-qpv = zeros(I, A);
-hpv = zeros(I, A);
-
-for a = [2, 20]
-    E = Xmc - T(:, 1:a) * P(:, 1:a)'; 
-    Epv = Xpvmc - Tpv(:, 1:a) * P(:, 1:a)'; 
-
-    q(:, a) = sum(E.^2, 2);
-    qpv(:, a) = sum(Epv.^2, 2);
-
-    h(:, a) = sum(T(:, 1:a).^2 ./ lambda(1:a), 2);
-    hpv(:, a) = sum(Tpv(:, 1:a).^2 ./ lambda(1:a), 2);
-end
+lambda = sum(T.^2) / (size(X, 1) - 1);
 
 % create distance plot for A = 2 and A = 20 similar to shown in the paper
-% but only for the new method
-figure
+% but only for the new method (see the code for function 'plotdistances' at the
+% end of this script)
 
-a = 2;
+figure
 subplot 121
-hold on
-scatter(h(:, a) / mean(h(:, a)), q(:, a) / mean(q(:, a)), 's')
-scatter(hpv(:, a) / mean(h(:, a)), qpv(:, a) / mean(q(:, a)), 'x')
-hold off
+plotdistances(P, lambda, Xmc, Xpvmc, 2)
 xlim([0, 20])
 ylim([0, 12])
-title("Distance plot (A = 2)")
-xlabel("Score distance, h/h0")
-ylabel("Orthogonal distance, q/q0")
-grid()
-box()
-legend(["cal", "pv"])
-
-a = 20;
 subplot 122
-hold on
-scatter(h(:, a) / mean(h(:, a)), q(:, a) / mean(q(:, a)), 's')
-scatter(hpv(:, a) / mean(h(:, a)), qpv(:, a) / mean(q(:, a)), 'x')
-hold off
+plotdistances(P, lambda, Xmc, Xpvmc, 20)
 xlim([0,  6])
 ylim([0, 12])
-title("Distance plot (A = 20)")
-xlabel("Score distance, h/h0")
-ylabel("Orthogonal distance, q/q0")
-grid()
-box()
-legend(["cal", "pv"])
+
 
 %% PCR based example
 
@@ -229,3 +194,32 @@ figure
 heatmap(D, 'Colormap', parula, 'ColorLimits', [-1, 2])
 xlabel("Components")
 ylabel("Segments, k")
+
+
+%% Helper functions
+
+function [h, q] = getdistances(P, lambda, X, A)
+%GETDISTANCES Compute score and orthogonal distance for PCA model and dataset X
+   T = X * P(:, 1:A);
+   E = X - T * P(:, 1:A)';
+   q = sum(E.^2, 2);
+   h = sum(T.^2 ./ lambda(1:A), 2);
+end
+
+function plotdistances(P, lambda, X, Xpv, A)
+%PLOTDISTANCES Show plot with score and orthogonal distance for calibration and PV-set
+   [h, q] = getdistances(P, lambda, X, A);
+   [hpv, qpv] = getdistances(P, lambda, Xpv, A);
+
+   hold on
+   scatter(h / mean(h), q / mean(q), 's')
+   scatter(hpv / mean(h), qpv / mean(q), 'x')
+
+   hold off
+   title(sprintf("Distance plot (A = %d)", A))
+   xlabel("Score distance, h/h0")
+   ylabel("Orthogonal distance, q/q0")
+   grid()
+   box()
+   legend(["cal", "pv"])
+end
