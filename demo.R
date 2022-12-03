e9ffe10cf3ab1157c34abbf6470a04c9f7ecbe2e
Sergey Kucheryavskiy <svkucheryavski@gmail.com>
2022-12-03 08:18:05 +0100

    R demo code has been updated to match the "pcv" packages

--- demo.R
+++ demo.R
@@ -1,6 +1,6 @@
 # This is demo code for the new version of the Procrustes cross-validation method (2022)
-# It requires "mdatools" package, if you do not have one
-# Run: install.packages("mdatools")
+# It requires "pcv" and "mdatools" packages, if you do not have one
+# Run: install.packages("pcv", "mdatools")
 #
 # The code creates plots similar to what you can see on Figures with Corn example
 # in the paper describing the new version, submitted in 2022. The Corn dataset was
@@ -9,31 +9,30 @@
 
 rm(list = ls())
 library(mdatools)
-source("pcv.R")
+library(pcv)
 
 # load original spectra and response values for Corn dataset
-load("corn.RData")
-
-# do SNV preprocessing
-x = prep.snv(x)
+data(corn)
+X = corn$spectra
+Y = corn$moisture
 
 # 1. PCA based examples
 
 ## create pseudo-validation set
-xpv <- pcvpca(x, ncomp = 30, cv = list("ven", 4))
+Xpv <- pcvpca(X, ncomp = 30, cv = list("ven", 4))
 
 ## show plot with original and generated spectra
 par(mfrow = c(2, 2))
-mdaplot(x, type = "l", cgroup = as.numeric(y), main = "Original")
-mdaplot(xpv, type = "l", cgroup = as.numeric(y), main = "Pseudo-validation")
-mdaplot(prep.autoscale(x), type = "l", cgroup = as.numeric(y), main = "Original (mean centered)")
-mdaplot(prep.autoscale(xpv), type = "l", cgroup = as.numeric(y), main = "Pseudo-validation (mean centered)")
+mdaplot(X, type = "l", cgroup = as.numeric(Y), main = "Original")
+mdaplot(Xpv, type = "l", cgroup = as.numeric(Y), main = "Pseudo-validation")
+mdaplot(prep.autoscale(X), type = "l", cgroup = as.numeric(Y), main = "Original (mean centered)")
+mdaplot(prep.autoscale(Xpv), type = "l", cgroup = as.numeric(Y), main = "Pseudo-validation (mean centered)")
 
 ## make PCA model for calibration set
-m <- pca(x, 20)
+m <- pca(X, 20)
 
 ## project pseudo-validation set to the model
-res_pv <- predict(m, xpv)
+res_pv <- predict(m, Xpv)
 
 ## show Distance plot for A = 2 and A = 20
 par(mfrow = c(1, 2))
@@ -44,10 +43,10 @@ plotResiduals(m, res = list(cal = m$res$cal, pv = res_pv), pch = c(22, 4), ylim 
 # 2. PLS based examples
 
 ## create pseudo-validation set for PLS model
-xpv <- pcvpls(x, y, ncomp = 30, cv = list("ven", 4))
+Xpv <- pcvpls(X, Y, ncomp = 30, cv = list("ven", 4))
 
 ## create PLS model using xpv as validation set
-m <- pls(x, y, ncomp = 20, x.test = xpv, y.test = y)
+m <- pls(X, Y, ncomp = 20, x.test = Xpv, y.test = Y)
 
 ## show the main plots
 par(mfrow = c(2, 2))
@@ -59,14 +58,12 @@ plotPredictions(m, 10)
 ## show heatmap for elements of D - it is returned as attribute of PC-set
 ## this plot must be identical to the first plot in the figure with heatmaps shown in the manuscript
 
-D <- attr(xpv, "D")
+D <- attr(Xpv, "D")
 
 colmap <- mdaplot.getColors(256, colmap = c("blue", "white", "red"))
 par(mfrow = c(2, 1))
-par(mar = c(1, 4, 2, 1))
-image(seq_len(30), seq_len(4), t(D), zlim = c(-2, 4), breaks = seq(-2, 4, length.out = 257),
-   col = colmap, xlab = "Components (a)", ylab = "Segments (k)", xaxt = "n", main = "K = 4")
-par(mar = c(5, 4, 1, 1))
+plotD(Xpv)
+
 boxplot(D, range = 100, col = "gray", border = "gray", ylim = c(0, 2), xlab = "Components", ylab = expression(c[k]/c))
 abline(h = 1, col = "black", lty = 2)
 par(mar = c(5, 4, 2, 2))
