4b88e7bf5e700550f722423adab48f7bddbc284a
Sergey Kucheryavskiy <svkucheryavski@gmail.com>
2023-07-06 10:32:05 +0200

    improvements to PCV for PCR implementation plus added tests

--- PCVMATLAB/pcvpcr.m
+++ PCVMATLAB/pcvpcr.m
@@ -1,4 +1,4 @@
-function [Xpv, D] = pcvpcr(X, Y, nComp, Center, Scale, CV)
+function [Xpv, D] = pcvpcr(X, Y, nComp, Center, Scale, CV, CVScope)
 %PCVPCR Compute matrix with pseudo-validation set for PCR model
 %
 % Arguments
@@ -9,13 +9,15 @@ function [Xpv, D] = pcvpcr(X, Y, nComp, Center, Scale, CV)
 % Center    logical, mean center data columns or not
 % Scale     logical, standardize data columns or not
 % CV        defines cross-validation splits pattern (see description)
+% CVScope   defines if centering/scaling must be done globally ('global')
+% or locally ('local'). Default value is 'global'.
 %
 % Returns:
 % --------
 % Xpv       matrix with PV-set (same size as X)
 % D         matrix with coefficients (ck/c) for each PC and each segment
 %
-% The method computes pseudo-validation matrix Xpv, based on PCR decomposition of 
+% The method computes pseudo-validation matrix Xpv, based on PCR decomposition of
 % calibration set (X, Y) and cross-validation. The 'CV' parameter can be:
 %
 % A number:
@@ -28,7 +30,7 @@ function [Xpv, D] = pcvpcr(X, Y, nComp, Center, Scale, CV)
 % 'CV = {"rand", n}' - random cross-validation with n segments
 % 'CV = {"ven", n}' - systematic cross-validation with n segments
 % 'CV = {"loo"}' - full cross-validation (leave one out)
-% 
+%
 % A vector:
 % =========
 % 'CV = [1, 2, 3, 1, 2, 3] - cross-validation of 6 rows with 3 segments
@@ -37,50 +39,70 @@ function [Xpv, D] = pcvpcr(X, Y, nComp, Center, Scale, CV)
 %
 % See also: PCVPCA, PCVPLS
 %
-    
-    % computes a global PCR model
-    function m = getglobalmodel(X, Y, nComp) 
-         [~, ~, P] = svd(X, 'econ');
-         P = P(:, 1:nComp);
-         Pi = eye(size(X, 2)) - P * P';
-         T = X * P;
-         C = (T' * T)' \ (T' * Y);
-         m = struct('P', P, 'C', C, 'Pi',  Pi, 'nComp', nComp);
-    end
 
-    % computes a local PCR model
-    function mk = getlocalmodel(Xc, Yc, m) 
-         [~, ~, Pk] = svd(Xc, 'econ');
-         Pk = Pk(:, 1:m.nComp);
-         aa = acos(sum(m.P .* Pk)) < (pi / 2);
-         Pk = Pk * diag(aa * 2 - 1);
-         Tc = Xc * Pk;
-         Ck = (Tc' * Tc)' \ (Tc' * Yc);
 
-         mk = struct('P', Pk, 'C', Ck); 
-    end
+   % default values
+   if nargin < 7
+      CVScope = "global";
+   end
 
-    % computes PV-set for current segment
-    function [Xpv, Dk] = getxpv(m, mk, Xk) 
+   if nargin < 6
+      CV = {"ven", 4};
+   end
 
-       Tk = Xk * mk.P;
-       Dk = diag(mk.C(:) ./ m.C(:));
-       Tpv = Tk * Dk;
-       Xpv = Tpv * m.P';
-    end
+   if nargin < 5
+      Scale = false;
+   end
 
-    % computes vector with orthogonal distances
-    function qk = getqk(Xk, mk) 
-         qk = sum( (Xk - Xk * mk.P * mk.P').^2, 2);
-    end
-    
-    funlist = struct(...
-        'getglobalmodel', @getglobalmodel, ...
-        'getlocalmodel', @getlocalmodel,...
-        'getxpv', @getxpv,...
-        'getqk', @getqk...
-    );
+   if nargin < 4
+      Center = true;
+   end
+   
+   if nargin < 3
+      nRows = size(X, 1);
+      nCols = size(X, 2);
+      nComp = min([nRows - 1, nCols, 30]);
+   end   
 
-    [Xpv, D] = pcvreg(X, Y, nComp, Center, Scale, CV, funlist);
+   % computes a global PCR model
+   function m = getglobalmodel(X, Y, nComp)
+      [~, ~, P] = svds(X, nComp);
+      Pi = eye(size(X, 2)) - P * P';
+      T = X * P;
+      C = (T' * T)' \ (T' * Y);
+      m = struct('P', P, 'C', C, 'Pi',  Pi, 'nComp', nComp);
+   end
+
+   % computes a local PCR model
+   function mk = getlocalmodel(Xc, Yc, m)
+      [~, ~, Pk] = svds(Xc, m.nComp);
+      aa = acos(sum(m.P .* Pk)) < (pi / 2);
+      Pk = Pk * diag(aa * 2 - 1);
+      Tc = Xc * Pk;
+      Ck = (Tc' * Tc)' \ (Tc' * Yc);      
+      mk = struct('P', Pk, 'C', Ck);
+   end
+
+   % computes PV-set for current segment
+   function [Xpv, Dk] = getxpv(m, mk, Xk)
+      Tk = Xk * mk.P;
+      Dk = diag(mk.C(:) ./ m.C(:));
+      Tpv = Tk * Dk;
+      Xpv = Tpv * m.P';
+   end
+
+   % computes vector with orthogonal distances
+   function qk = getqk(Xk, mk)
+      qk = sum( (Xk - Xk * mk.P * mk.P').^2, 2);
+   end
+
+   funlist = struct(...
+      'getglobalmodel', @getglobalmodel, ...
+      'getlocalmodel', @getlocalmodel,...
+      'getxpv', @getxpv,...
+      'getqk', @getqk...
+      );
+
+   [Xpv, D] = pcvreg(X, Y, nComp, Center, Scale, CV, funlist, CVScope);
 end
 
